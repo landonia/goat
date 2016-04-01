20e2470058b16278caac51ffb129cb3d9adaf983
Landon Wainwright <landon.wainwright@bet365.com>
2016-04-01 10:09:31 +0100

    Version 1

---
+++ README.md
@@ -0,0 +1,60 @@
+# keystore
+
+A simple wrapper for creating a ticker execution of a function.
+
+## Overview
+
+Goat provides the core `for/select` wrapper that is used to safely execute within
+a go routine using a ticker to signal execution of a function.
+The function provided will be executed at the ticker duration.
+A goat can be shutdown cleanly. A Goat can be reused - if it has been shutdown
+it can be started again without creating a new Goat.
+
+## Maturity
+
+Ready to go and is used in production
+
+## Installation
+
+simply run `go get github.com/landonia/goat`
+
+## Use as Library
+
+	package main
+
+	import (
+  		"github.com/landonia/goat"
+  	)
+
+  	func main() {
+
+       // Create a new goat
+       g := goat.New(time.Minute,
+         func() {
+           fmt.Println("Executed")
+         },
+       )
+
+       // Start the execution
+       err := g.Start()
+       if err != nil {
+         // It means that it has already been started
+         fmt.Println("Already started")
+       }
+
+       // Do work
+       ...
+
+       // Ready to shutdown
+       err = g.Stop()
+       if err != nil {
+         // It means that it was not running
+         fmt.Println("Not started")
+       }
+  	}
+
+## About
+
+goat was written by [Landon Wainwright](http://www.landotube.com) | [GitHub](https://github.com/landonia).
+
+Follow me on [Twitter @landoman](http://www.twitter.com/landoman)!
---
+++ goat.go
@@ -0,0 +1,69 @@
+// Copyright 2016 Landon Wainwright.
+
+// Package goat provides a go asynchronous ticker wrapper
+package goat
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// Goat is the type for the interval executioner
+type Goat struct {
+	duration time.Duration
+	handler  func()
+	exit     chan bool
+	r        bool
+	mut      sync.Mutex
+}
+
+// New will create a new goat that will call the function at every
+// duration interval.
+func New(duration time.Duration, handler func()) *Goat {
+	return &Goat{
+		duration: duration,
+		handler:  handler,
+		exit:     make(chan bool),
+	}
+}
+
+// Start will start the ticker execution routine if it is not currently executing
+func (goat *Goat) Start() error {
+	goat.mut.Lock()
+	defer goat.mut.Unlock()
+	if goat.r {
+		return fmt.Errorf("Already executing - Stop first to start a new goat")
+	}
+	goat.r = true
+
+	// Launch the routine for handling the ticker execution
+	go func() {
+		ticker := time.NewTicker(time.Minute)
+		for {
+			select {
+			case <-goat.exit:
+				ticker.Stop()
+				return
+			case <-ticker.C:
+
+				// Call the handler
+				goat.handler()
+			}
+		}
+	}()
+	return nil
+}
+
+// Stop will stop the execution routine and will block until the shutdown is
+// received
+func (goat *Goat) Stop() error {
+	goat.mut.Lock()
+	defer goat.mut.Unlock()
+	if !goat.r {
+		return fmt.Errorf("Not executing - Start first to stop a goat")
+	}
+	goat.exit <- true
+	goat.r = false
+	return nil
+}
